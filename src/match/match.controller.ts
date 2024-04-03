e14e694b4df5877a2f25fa780511dc9caa002fb3
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-04-03 14:40:47 +0800

    Match get. Match send message

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,19 +1,45 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { MatchService } from './match.service';
+import { ApiKeyGuard } from 'src/auth/api-key.guard';
+import { CurrentUser } from 'src/auth/current-user';
+import { User } from '@prisma/client';
 
 @Controller('match')
 export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
+  @Get('list')
+  async getMatches() {
+    return await this.matchService.getMatches();
+  }
+
   // TODO: guard by admin
   @Post('create')
-  createMatch(
+  async createMatch(
     @Body() body: { name: string; playerOneId: number; playerTwoId: number },
   ) {
-    return this.matchService.createMatch(
+    return await this.matchService.createMatch(
       body.name,
       body.playerOneId,
       body.playerTwoId,
+    );
+  }
+
+  @Get(':id')
+  async getMatch(@Param('id') id: string) {
+    return await this.matchService.getMatch(id);
+  }
+
+  @UseGuards(ApiKeyGuard)
+  @Post('sendmsg')
+  async sendMessage(
+    @CurrentUser() user: User,
+    @Body() body: { matchId: number; message: string },
+  ) {
+    return await this.matchService.sendMessage(
+      body.matchId,
+      user,
+      body.message,
     );
   }
 }
--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MatchService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async getMatches() {
+    return await this.prismaService.match.findMany();
+  }
 
   async createMatch(name: string, playerOneId: number, playerTwoId: number) {
     const match = await this.prismaService.match.create({
@@ -16,5 +21,20 @@ export class MatchService {
     });
 
     return match;
+  }
+
+  async getMatch(_id: string) {
+    const id: number = parseInt(_id);
+    return await this.prismaService.match.findUnique({ where: { id: id } });
+  }
+
+  async sendMessage(matchId: number, user: User, message: string) {
+    return await this.prismaService.message.create({
+      data: {
+        matchId: matchId,
+        userId: user.id,
+        text: message,
+      },
+    });
   }
 }

c56499fc8712a592dea22736a446121d0de655ee
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-04-03 15:37:11 +0800

    Update prisma client. Port env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:3000'] });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();

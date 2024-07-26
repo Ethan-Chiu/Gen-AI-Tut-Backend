ccbde3ff98702fa3e277d045dbf8a391739e18c4
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-07-26 23:01:43 +0800

    Cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://localhost:4173',
+    ],
   });
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
